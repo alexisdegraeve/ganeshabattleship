4d865f87941f74eeff65de3061cb4237d7f00da0
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-12-12 12:56:32 +0100

    init project angular

---
+++ ganeshabattleship/src/ship-placement/ship-placement.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ship-placement',
+  imports: [],
+  templateUrl: './ship-placement.html',
+  styleUrl: './ship-placement.scss',
+})
+export class ShipPlacement {
+
+}
