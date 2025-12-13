b4b28ef826b8964ba6bba28a4816df6b501f7d60
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-12-13 17:47:48 +0100

    add instruction add welcome

--- ganeshabattleship/src/app/app.routes.ts
+++ ganeshabattleship/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from '../game/game';
 import { ShipPlacementComponent } from '../ship-placement/ship-placement';
+import { WelcomeComponent } from '../welcome/welcome';
+import { InstructionComponent } from '../instruction/instruction';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'ship-placement', pathMatch: 'full' }, // redirection vers l'accueil
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' }, // redirection vers l'accueil
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'instruction', component: InstructionComponent },
   { path: 'ship-placement', component: ShipPlacementComponent },
   { path: 'game', component: GameComponent },
   { path: '**', redirectTo: 'home' } // fallback pour les routes inconnues
---
+++ ganeshabattleship/src/instruction/instruction.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instruction',
+  imports: [],
+  templateUrl: './instruction.html',
+  styleUrl: './instruction.scss',
+})
+export class InstructionComponent {
+
+}
